4a8eea24d7917b273d2ccd672a562520bb616351
Yan Marinho <yan.marinho@pixter.com.br>
2022-10-16 11:14:35 -0300

    ✨ creating input and button to insert todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
+import { ChangeEvent, useState } from "react";
 import { Header } from "./components";
+import { PlusCircle } from "phosphor-react";
 import style from "./App.module.css";
 
 function App() {
+  const [taskName, setTaskName] = useState<string>("");
+
+  const hadleGenerateNewTask = () => {
+    setTaskName("");
+  };
+
+  const handleSetNewTaskName = (event: ChangeEvent<HTMLInputElement>) =>
+    setTaskName(event.target.value);
+
   return (
     <div className={style.container}>
       <header>
         <Header />
+        <section className={style.listInformation}>
+          <input
+            value={taskName}
+            onChange={handleSetNewTaskName}
+            placeholder="Adicione uma nova tarefa"
+            type="text"
+          />
+          <button onClick={() => hadleGenerateNewTask()}>
+            Criar
+            <PlusCircle size={18} />
+          </button>
+        </section>
       </header>
     </div>
   );
